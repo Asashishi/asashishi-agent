034de67a6d8e7d1f25c145bdf5f123586be78240
asashishi <assassinjpwh@outlook.com>
2025-12-14 12:42:42 +0800

    built‑in system prompt

--- conf/init-conf.go
+++ conf/init-conf.go
@@ -10,11 +10,10 @@ var Env EnvConfig = EnvConfig{}
 
 func InitConfig() {
 	var (
-		err       error
-		dirName   string
-		sysPrompt []byte
-		file      *os.File
-		decoder   *json.Decoder
+		err     error
+		dirName string
+		file    *os.File
+		decoder *json.Decoder
 	)
 	if dirName, err = os.Getwd(); err != nil {
 		panic(err)
@@ -26,9 +25,5 @@ func InitConfig() {
 	if err = decoder.Decode(&Env); err != nil {
 		panic(err)
 	}
-	// prompt
-	if sysPrompt, err = os.ReadFile(path.Join(dirName, SysPromptName)); err != nil {
-		panic(err)
-	}
-	Env.SysPrompt = string(sysPrompt)
+	Env.SysPrompt = SysPrompt
 }
