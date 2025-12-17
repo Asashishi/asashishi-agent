6da4f4b9d2705aef8e68457868574ce6e7d682cb
asashishi <assassinjpwh@outlook.com>
2025-12-17 15:50:34 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
-set -e  # 出错立即退出
-
+set -e
 echo "-- Start"
-
 echo "rm -rf build"
 rm -rf build
 mkdir -p build
-
 echo "go mod tidy"
 go mod tidy
-
-# rsrc 工具在 Linux 下也能用，如果需要生成 .syso 文件可以保留
 echo "rsrc -ico ./resources/app.ico -o app.syso"
 rsrc -ico ./resources/app.ico -o app.syso
-
 echo "go build -ldflags=\"-s -w\" -trimpath -o asashishi-agent"
 go build -ldflags="-s -w" -trimpath -o asashishi-agent
-
 echo "rm -f app.syso"
 rm -f app.syso
-
-# 压缩可执行文件
 echo "upx --best --lzma asashishi-agent"
 upx --best --lzma asashishi-agent
-
 mv asashishi-agent build/
 cp README.md build/
 cp config.json build/
-
-echo "zip -r build/linux-x86-amd64.zip build"
-zip -r build/linux-x86-amd64.zip build
-
+echo "tar -czf linux-x86-amd64.gz build"
+tar -czf linux-x86-amd64.gz build
+mv linux-x86-amd64.gz build/
 echo "Complete!"
