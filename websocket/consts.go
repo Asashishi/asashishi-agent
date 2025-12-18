ae6fd6ccce77fed759bbf02eef438cfaf96d5eba
asashishi <assassinjpwh@outlook.com>
2025-12-18 14:03:54 +0800

    code optimize

--- websocket/consts.go
+++ websocket/consts.go
@@ -3,8 +3,8 @@ package websocket
 const WebsocketURL string = "ws://localhost:%d/ws"
 const ProcessExit string = "Process Exit"
 
-const AIOutputType string = "AI-Msg"
-const SystMsgType string = "Sys-Msg"
-const SystWarnType string = "Sys-Warn"
-const SysErrorType string = "Sys-Error"
-const UserInputType string = "User-Input"
+const AIOutputType string = "ai_msg"
+const SystMsgType string = "sys_msg"
+const SystWarnType string = "sys_warn"
+const SysErrorType string = "sys_error"
+const UserInputType string = "user_input"
