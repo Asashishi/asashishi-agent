19151c8027708b1df2643cfc72673a8ff898f845
asashishi <assassinjpwh@outlook.com>
2025-12-19 11:14:15 +0800

    code optimize

--- cmd/styled-text.go
+++ cmd/styled-text.go
@@ -4,6 +4,5 @@ import (
 	"asashishi-agent/global"
 )
 
-var FileContentMark string = global.LineBreakString + global.GetStyledSystemComent("-- File Content:")
-
 var EOFMark string = global.LineBreakString + global.GetStyledSystemComent("-- EOF")
+var FileContentMark string = global.LineBreakString + global.GetStyledSystemComent("-- File Content:")
--- tools/consts.go
+++ tools/consts.go
@@ -2,5 +2,5 @@ package tools
 
 const StopedByUser string = "\nStoped by user"
 const TimeFormatString string = "20060102150405"
-const WindowsInitialCommand string = "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8"
 const LinuxInitialCommand string = "export LANG=en_US.UTF-8"
+const WindowsInitialCommand string = "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8"
