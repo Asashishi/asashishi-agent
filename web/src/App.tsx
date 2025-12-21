ce5f8e7f4888b5e1e5282cdc9e582d86590df8fd
asashishi <assassinjpwh@outlook.com>
2025-12-21 21:30:55 +0800

    code optimize

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,15 @@
 import { useState, type ChangeEvent, type JSX } from 'react'
-import type { ContextStorageItem, WebSocketMsg } from './types';
 import { MainPageContext } from './context';
+import AsashishiAgentWs from './utils/websocket';
 
-// 无论使用框架还是原生 实现 websocket 回调和双向绑定即可完成实时数据交互和展示
-let socket: WebSocket = new WebSocket("ws://localhost:3000/ws");
+const wsInstance: AsashishiAgentWs = new AsashishiAgentWs();
 
-const displayMsg = (data: WebSocketMsg): void => {
-  if (data.type === "exec_output") {
-    const scpOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("scpOutput");
-    scpOutputState?.setValue(scpOutputState.value + data.content + "\n");
-  } else {
-    const aiOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("aiOutput");
-    aiOutputState?.setValue(aiOutputState.value + data.content);
-  }
-}
-
-socket.onopen = () => {
-  console.log("✅ 已连接到 WebSocket 服务");
-};
-socket.onmessage = function(event: MessageEvent<string>) {
-  const tAraeState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("tAraeValue");
-  tAraeState?.setValue("");
-  displayMsg(JSON.parse(event.data));
-};
-socket.onclose = function() {
-  console.log("\n ❌ 连接已关闭");
-};
-socket.onerror = function(error) {
-  console.error("\n ⚠️ 出错: " + error);
-  // 如果是无关紧要的出错(如网络) 重连可以解决问题 服务器仅维护一个 socket 对象 不支持多个连接 如果需要可以自己改写
-  socket = new WebSocket("ws://localhost:3000/ws");
+const injectContextItems = (): void => {
+  wsInstance.injectContextItems(
+    MainPageContext.get("tAraeValue")!,
+    MainPageContext.get("aiOutput")!,
+    MainPageContext.get("scpOutput")!,
+  );
 };
 
 const App: React.FC = (): JSX.Element => {
@@ -60,6 +40,9 @@ const App: React.FC = (): JSX.Element => {
       }
     }
   ]);
+
+  injectContextItems();
+
   return (
     <div>
       <h2>WebSocket Example</h2>
@@ -75,7 +58,7 @@ const App: React.FC = (): JSX.Element => {
           />
         </div>
         <br />
-        <button onClick={() => socket.send(
+        <button onClick={() => wsInstance.send(
           JSON.stringify({
             type: 'user_input',
             content: tAraeValue,
