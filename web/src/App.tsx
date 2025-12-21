0a84e537656616944d727658d0bfc41b405d2fa0
asashishi <assassinjpwh@outlook.com>
2025-12-21 15:48:35 +0800

    update comment

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,19 @@ import { useState, type ChangeEvent, type JSX } from 'react'
 import type { ContextStorageItem, WebSocketMsg } from './types';
 import { MainPageContext } from './context';
 
-
+// 无论使用框架还是原生 实现 websocket 回调和双向绑定即可完成实时数据交互和展示
 let socket: WebSocket = new WebSocket("ws://localhost:3000/ws");
+
+const displayMsg = (data: WebSocketMsg): void => {
+  if (data.type === "exec_output") {
+    const scpOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("scpOutput");
+    scpOutputState?.setValue(scpOutputState.value + data.content + "\n");
+  } else {
+    const aiOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("aiOutput");
+    aiOutputState?.setValue(aiOutputState.value + data.content);
+  }
+}
+
 socket.onopen = () => {
   console.log("✅ 已连接到 WebSocket 服务");
 };
@@ -20,16 +31,6 @@ socket.onerror = function(error) {
   // 如果是无关紧要的出错(如网络) 重连可以解决问题 服务器仅维护一个 socket 对象 不支持多个连接 如果需要可以自己改写
   socket = new WebSocket("ws://localhost:3000/ws");
 };
-
-const displayMsg = (data: WebSocketMsg): void => {
-  if (data.type === "exec_output") {
-    const scpOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("scpOutput");
-    scpOutputState?.setValue(scpOutputState.value + data.content + "\n");
-  } else {
-    const aiOutputState: ContextStorageItem<string> | undefined = MainPageContext.get<string>("aiOutput");
-    aiOutputState?.setValue(aiOutputState.value + data.content);
-  }
-}
 
 const App: React.FC = (): JSX.Element => {
   const [aiOutput, setAiOutput] = useState<string>("");
